71df919c235c3623361d5189ad54143c3e9c07b7
Alexis <alexis.breuse@gmail.com>
2019-01-14 20:17:08 +0100

    add some logic to understand better

--- src/alexis/breuse/ATM.java
+++ src/alexis/breuse/ATM.java
@@ -2,14 +2,26 @@ package alexis.breuse;
 
 public class ATM {
 
+    private int totalAmountWithdrawn;
+
     private static ATM instance;
 
-    private ATM() {}
+    private ATM() {
+        this.totalAmountWithdrawn = 0;
+    }
 
     public static ATM getInstance() {
         if(instance == null)
             instance = new ATM();
 
         return instance;
+    }
+
+    public void withdraw(int amount) {
+        this.totalAmountWithdrawn += amount;
+    }
+
+    public void printTotalAmountWithdrawn() {
+        System.out.println(this.totalAmountWithdrawn);
     }
 }
--- src/alexis/breuse/Main.java
+++ src/alexis/breuse/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	    ATM atm = ATM.getInstance();
+	    atm.withdraw(20);
+
+	    ATM atm2 = ATM.getInstance();
+	    atm.withdraw(30);
+
+	    //atm & atm2 = same instance
+	    atm.printTotalAmountWithdrawn();
+	    atm2.printTotalAmountWithdrawn();
     }
 }
